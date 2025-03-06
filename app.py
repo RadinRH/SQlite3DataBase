9cd4b57852e8e39e55c7af5a3d5131a5039646ef
RadinRH <radinrh23@gmail.com>
2025-03-06 19:07:36 +0330

    add multiple insert in database

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import sqlite3
 
-def db_manager(q):
+def db_manager_single(q):
     connection = sqlite3.connect('myshop.db')
     cursor = connection.cursor()
     cursor.execute(q)
+    connection.commit()
+    connection.close()
+
+def db_manager_many(q, data):
+    connection = sqlite3.connect('myshop.db')
+    cursor = connection.cursor()
+    cursor.executemany(q, data)
     connection.commit()
     connection.close()
 
@@ -17,8 +24,7 @@ q = '''
         password TEXT NOT NULL
     )
 '''
-db_manager(q)
-
+# db_manager_single(q)
 
 
 # Insert into users
@@ -27,13 +33,27 @@ q = '''
     VALUES ("jondo", "john doe", "123")
 '''
 
-
 q = '''
     INSERT INTO users (user_name, full_name, password)
     VALUES ("radin", "hedayati", "321")
 '''
-db_manager(q)
+# db_manager_single(q)
 
+
+# Insert multiple value to user table
+data = [
+    ("ali@gmail.com", "Ali Akbari", "123"),
+    ("sara@gmail.com", "Sara Ahadi", "321"),
+    ("ben@gmail.com", "Ben Mira", "333"),
+    ("saeed@gmail.com", "Saeed Sobhan", "4444"),
+    ("maryam@gmail.com", "Maryam Imani", "1111"),
+]
+
+q = '''
+    INSERT INTO users (user_name, full_name, password)
+    VALUES(?, ?, ?)
+'''
+db_manager_many(q, data)
 
 
 
